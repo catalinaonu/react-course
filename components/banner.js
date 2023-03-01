e5e1623c2ba6b4fda8b52b830ee821a45c1fae3e
catalinaonu <55512833+catalinaonu@users.noreply.github.com>
2023-03-01 15:08:36 +0100

    1 binaries uploaded

--- components/banner.js
+++ components/banner.js
@@ -2,7 +2,7 @@ const Banner = () => {
   return (
     <header>
       <div>
-        <img src="house.png" alt="house" />
+        <img src="./house.jpg" alt="house" />
       </div>
       <div>Providing houses all over the world</div>
     </header>
